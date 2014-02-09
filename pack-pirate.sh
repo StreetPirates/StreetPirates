796be7aef3d98ccf11ffc9924e8e5c2337f93dd1
Vasilis Liaskovitis <vliaskov@gmail.com>
2014-02-09 02:43:58 +0200

    Add pack-pirate.sh script

---
+++ pack-pirate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+jarfile=$1
+jar uf $jarfile assets/city/kth* 
+jar uf $jarfile assets/*compass*tmx
+jar uf $jarfile assets/city/city*ct28* 
+jar uf $jarfile assets/city/sea.jpg
+jar uf $jarfile assets/city/bandits*png
+jar uf $jarfile assets/city/map*png
+jar uf $jarfile assets/pirate/*png 
+jar uf $jarfile assets/cars/red*png 
+jar uf $jarfile assets/cars/green*png 
+jar uf $jarfile assets/cars/blue*png 
+jar uf $jarfile assets/menu/*png 
+jar uf $jarfile assets/menu/*jpg
+jar uf $jarfile assets/map/footstepsblue.png
+jar uf $jarfile assets/map/trafficLight.png
+jar uf $jarfile assets/map/parrot_front.jpg
+jar uf $jarfile assets/map/Map*png
+jar uf $jarfile assets/map/map*png
+jar uf $jarfile assets/map/pirateflag.png
+jar uf $jarfile assets/map/treasure1.png
+jar uf $jarfile assets/map/compass.png
+jar uf $jarfile assets/map/starfish-alpha.png
+jar uf $jarfile assets/storytelling/*jpg
+jar uf streetpiratesv0.2.jar assets/ui/uiskin.json
+jar uf streetpiratesv0.2.jar assets/ui/default.fnt
